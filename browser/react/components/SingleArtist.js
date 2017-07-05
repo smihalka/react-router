999d4d7073ab9a6a35b6a805062670c947965b3d
Shayne Mihalka <shayne@mihalka.com>
2017-07-05 15:31:44 -0700

    Data From the Server

--- browser/react/components/SingleArtist.js
+++ browser/react/components/SingleArtist.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class SingleArtist extends Component {
+  constructor(){
+    super();
+    this.state = {
+      artist: {},
+      artistAlbums: [],
+      artistSongs: []
+    }
+  }
+
+  componentDidMount() {
+  //  console.log(this.props.match.params.artistId)
+   axios.get(`/api/artists/${this.props.match.params.artistId}`)
+      .then(res => res.data)
+      .then(artist => {
+
+         this.setState({artist})
+       })
+  }
   render() {
+    console.log(this.state.artist.name)
     return (
       <div>
-        <h3>ARTIST NAME</h3>
+        <h3></h3>
         <h4>ALBUMS</h4>
         <h4>SONGS</h4>
       </div>
