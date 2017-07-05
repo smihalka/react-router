873a8752c49c68d63835f1224377e1e5fa2b6712
Min Hwangbo <min.hwangbo@gmail.com>
2017-07-05 16:49:18 -0400

    4 Moving our State

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -4,25 +4,17 @@ import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
+import { HashRouter, Route } from 'react-router-dom';
 
 export default class Main extends Component {
 
   constructor (props) {
     super(props);
     this.state = {
-      albums: [],
       selectedAlbum: {}
     };
     this.selectAlbum = this.selectAlbum.bind(this);
     this.deselectAlbum = this.deselectAlbum.bind(this);
-  }
-
-  componentDidMount () {
-    axios.get('/api/albums/')
-      .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
   }
 
   selectAlbum (albumId) {
@@ -43,13 +35,17 @@ export default class Main extends Component {
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
-        <div className="col-xs-10">
-        {
+        <HashRouter>
+          <div className="col-xs-10">
+          <Route exact path="/" component={AllAlbums} />
+          <Route path="/albums" component={AllAlbums} />
+            {/*
           this.state.selectedAlbum.id ?
           <SingleAlbum album={this.state.selectedAlbum} /> :
           <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
-        </div>
+        */}
+          </div>
+        </HashRouter>
         <Player />
       </div>
     );
