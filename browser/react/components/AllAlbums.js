03e92bb8a437808ce42a7d5ec735a8e3c4c15006
Shayne Mihalka <shayne@mihalka.com>
2017-07-05 14:41:22 -0700

    Link Need a Router

--- browser/react/components/AllAlbums.js
+++ browser/react/components/AllAlbums.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class AllAlbums extends Component {
 
@@ -30,6 +31,7 @@ export default class AllAlbums extends Component {
         {
           albums.map(album => (
             <div className="col-xs-4" key={ album.id }>
+            {/*}  <Link to={`/albums/${album.id}`}</Link>*/}
               <a className="thumbnail" href="#" onClick={() => selectAlbum(album.id)}>
                 <img src={ album.imageUrl } />
                 <div className="caption">
