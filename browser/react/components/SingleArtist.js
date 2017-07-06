4fec040cb6da620dccb1f7c656d7a488a1bc8dca
Min Hwangbo <min.hwangbo@gmail.com>
2017-07-06 11:40:42 -0400

    added multiple axios requests under promise

--- browser/react/components/SingleArtist.js
+++ browser/react/components/SingleArtist.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+const Promise = require('bluebird');
 
 export default class SingleArtist extends Component {
   constructor(){
@@ -13,18 +14,41 @@ export default class SingleArtist extends Component {
 
   componentDidMount() {
   //  console.log(this.props.match.params.artistId)
-   axios.get(`/api/artists/${this.props.match.params.artistId}`)
-      .then(res => res.data)
-      .then(artist => {
+  const artistNames = axios.get(`/api/artists/${this.props.match.params.artistId}`);
+  const artistAlbums = axios.get(`/api/artists/${this.props.match.params.artistId}/albums`);
+  const artistSongs = axios.get(`/api/artists/${this.props.match.params.artistId}/songs`);
 
-         this.setState({artist})
-       })
+  // console.log(this);
+
+   Promise.all([artistNames, artistAlbums, artistSongs])
+   .spread((name, albums, songs) => {
+    //  console.log(this)
+     return [name.data, albums.data, songs.data]
+   })
+   .spread((dbName, dbAlbums, dbSongs) => {
+    // console.log(this);
+     this.setState({
+       artist: dbName,
+       artistAlbums: dbAlbums,
+       artistSongs: dbSongs
+     })
+    //  console.log(this.state);
+   })
+      // .then(res => res.data)
+      // .then(artist => {
+      //    console.log(artist.name);
+      //    this.setState({
+      //      artist: artist.name
+      //    })
+      //  })
+
   }
+
   render() {
     console.log(this.state.artist.name)
     return (
       <div>
-        <h3></h3>
+        <h3>{this.state.artist.name}</h3>
         <h4>ALBUMS</h4>
         <h4>SONGS</h4>
       </div>
